1e80d609be2b19a7ce7dc9c9c76aff24d3b90d4e
ice100k <61597736+ice100k@users.noreply.github.com>
2020-03-27 10:44:49 +0000

    Update block.js

--- adblock/block.js
+++ adblock/block.js
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-firefox.webRequest.onBeforeRequest.addListener(
-=======
 browser.webRequest.onBeforeRequest.addListener(
->>>>>>> 93e5fbba615f7137013cb888803805b8716cce6e
 	function(details) {
 		console.log("blocking:", details.url);
 		return {cancel: true };
