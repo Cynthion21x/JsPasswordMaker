63823abbbaf923af6e7bd876bce50c73d65400fe
thecrisperson1 <62443519+thecrisperson1@users.noreply.github.com>
2020-03-27 11:45:10 +0000

    Update block.js

--- adblock/block.js
+++ adblock/block.js
@@ -1,9 +1,7 @@
-<<<<<<< HEAD:block.js
 <<<<<<< HEAD
 =======
-=======
->>>>>>> :adblock/block.js
 browser.webRequest.onBeforeRequest.addListener(
+>>>>>>> 93e5fbba615f7137013cb888803805b8716cce6e
 	function(details) {
 		console.log("blocking:", details.url);
 		return {cancel: true };
