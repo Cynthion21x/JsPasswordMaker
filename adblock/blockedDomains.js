a3d20badbada273a675151c68659b0364a27995e
ice100k <61597736+ice100k@users.noreply.github.com>
2020-03-27 11:30:27 +0000

    Add files via upload

---
+++ adblock/blockedDomains.js
@@ -0,0 +1,7 @@
+var blocked_domains = [
+    "*://*.lb.usemaxserver.de/*",
+    "*://*.tracking.klickthru.com/*",
+    "*://*.zmedia.com/*",
+    "*://*.doubleclick.net/*",
+    "*://*.telegraph.co.uk/*",
+    "*://*.zv1.november-lax.com/*"];
